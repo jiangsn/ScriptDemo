4a475ade7312f8a9503072e332215d110b4d937a
jiangxue <994898372@qq.com>
2018-10-28 17:02:59 +0800

    update

--- nodeScript.js
+++ nodeScript.js
@@ -5,12 +5,11 @@ let fs = require("fs")
 var dirName = process.argv[2]
 
 fs.mkdirSync("./" + dirName)
-process.chdir("./" + dirName)
-fs.mkdirSync('css')
-fs.mkdirSync('js')
+fs.mkdirSync(dirName+'/css')
+fs.mkdirSync(dirName+'/js')
 
-fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
-fs.writeFileSync("css/style.css", "h1{color: red;}")
-fs.writeFileSync("./js/main.js", "var string = 'Hello World'\nalert(string)")
+fs.writeFileSync(dirName+"/index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+fs.writeFileSync(dirName+"/css/style.css", "h1{color: red;}")
+fs.writeFileSync(dirName+"/js/main.js", "var string = 'Hello World'\nalert(string)")
 
 process.exit(0)
